f92f5a6268b16144fed03ba86397685f74e5b170
Tracy Le <tnle9@asu.edu>
2021-06-07 12:10:54 -0700

    Added responsive styling
    Shop fully styled and working

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,14 @@
-// Put your application javascript here
+// initialize your js when the document is ready
+
+$(document).ready(function() {
+
+    $(document).on('change', '#variant-id', function() {
+        // write a function to get the active value of the selected box
+        let 
+            variant_id = this.value, // get the selected image id
+            variant_image = $('.single-product-image[data-variant="' + variant_id + '"]');  // grabs the div with the specific class that has the selected variant id
+
+        variant_image.show().siblings(':visible').hide(); // show selected image and hide any active visible sibling images
+    })
+
+});
